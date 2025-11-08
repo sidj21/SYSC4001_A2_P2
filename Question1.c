3444d0fcf0f64376ae8c070a4e36b3efc843d85a
AJSRI142 <133076429+AJSRI142@users.noreply.github.com>
2025-11-07 21:30:10 -0500

    Change delay in process counters to 2 seconds
    Updated the delay in both child and parent processes from 0.5 seconds to 2 seconds.

--- Question1.c
+++ Question1.c
@@ -2,32 +2,31 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h> // To be able to use usleep for the delay
- 
+#include <unistd.h> // Used for any process related system calls  
+ // usleep provides a delay in microseconds between each counter increment
 int main() {
-    pid_t pid; 
+    pid_t pid; // pid_t is a data type for process IDs in Linux  
     
     pid = fork(); 
         if (pid < 0){
             perror("fork() failed"); 
             exit(1);
+        }
         if (pid == 0){
             int count_child = 0; 
             while(1){
                 printf("Child process counter: %d\n" , count_child++);  
-                usleep(50000); // Creates a delay of 0.5 secs
+                usleep(2000000); // Creates a delay of 2 secs
             }
         }
         else{
             int count_parent = 0;            
             while(1){
                 printf("Parent process counter: %d\n", count_parent++); 
-                usleep(50000); // Creates a delay of 0.5 secs
+                usleep(2000000); // Creates a delay of 2 secs
 
             }
         }
-    }
-    
     
     return 0;
 }
